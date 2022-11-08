83a4ac2bf48945ef6e0992b60fd3e875e0ed76bb
JesusRiestra <jriestraricote@gmail.com>
2022-11-08 09:45:11 +0100

    Permitiendo reducir el precio del billete

--- MaquinaExpendedoraSimple.java
+++ MaquinaExpendedoraSimple.java
@@ -53,6 +53,10 @@ public class MaquinaExpendedoraSimple {
     public void setPrecioBillete(int precioDelBillete) {
         precioBillete = precioDelBillete;
     }
+    
+    public void reducirPrecioBillete(int precioReducido) {
+        precioBillete = precioBillete - precioReducido;
+    }
 
     /**
      * Simula la introduccion de dinero por parte del cliente actual
